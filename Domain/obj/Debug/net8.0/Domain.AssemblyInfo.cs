eac782a2f763922466772f1dc1349f77ce3cfef6
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-06-19 13:44:22 +0200

    seeding data to the database

--- Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
+++ Domain/obj/Debug/net8.0/Domain.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+68a1eb14c30c460c344f2b5957f0aee80af8b68a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fc549c5e9313168dc6b39890d511a3a5b3027ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
