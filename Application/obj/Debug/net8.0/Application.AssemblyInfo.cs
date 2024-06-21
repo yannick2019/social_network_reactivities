ca4005d9630dacb2f91539ad90545082baf46f13
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-06-21 12:52:51 +0200

    setup client-app

--- Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eac782a2f763922466772f1dc1349f77ce3cfef6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7367fa8ca0005870e039c0357072490ef447bc02")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
