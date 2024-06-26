8e937024ba08ccc3673580c8e71f89e324241718
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-06-26 14:32:50 +0200

    feat: create a details view

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridColumn } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activities";
 import ActivityList from "./ActivityList";
+import ActivityDetails from "../details/ActivityDetails";
 
 interface Props {
   activities: Activity[];
@@ -11,6 +12,9 @@ export default function ActivityDashboard({ activities }: Props) {
     <Grid>
       <GridColumn width="10">
         <ActivityList activities={activities} />
+      </GridColumn>
+      <GridColumn width="6">
+        {activities[0] && <ActivityDetails activity={activities[0]} />}
       </GridColumn>
     </Grid>
   );
---
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -0,0 +1,36 @@
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardMeta,
+  Image,
+} from "semantic-ui-react";
+import { Activity } from "../../../app/models/activities";
+
+interface Props {
+  activity: Activity;
+}
+
+export default function ActivityDetails({ activity }: Props) {
+  return (
+    <Card fluid>
+      <Image src={`/assets/categoryImages/${activity.category}.jpg`} />
+      <CardContent>
+        <CardHeader>{activity.title}</CardHeader>
+        <CardMeta>
+          <span className="date">{activity.date}</span>
+        </CardMeta>
+        <CardDescription>{activity.description}</CardDescription>
+      </CardContent>
+      <CardContent extra>
+        <ButtonGroup widths="2">
+          <Button basic color="blue" content="Edit" />
+          <Button basic color="grey" content="Cancel" />
+        </ButtonGroup>
+      </CardContent>
+    </Card>
+  );
+}
