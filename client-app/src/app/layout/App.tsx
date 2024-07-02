1342b2ff5d3d0862feba40274e18fef97a8d52b1
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-07-02 14:53:52 +0200

    setting up Axios

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,77 +1,79 @@
 import { useEffect, useState } from "react";
-import axios from "axios";
 import { Container } from "semantic-ui-react";
 import { Activity } from "../models/activities";
 import NavBar from "./NavBar";
 import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
 import { v4 as uuid } from "uuid";
+import agent from "../api/agent";
 
 function App() {
-  const [activities, setActivities] = useState<Activity[]>([]);
-  const [selectedActivity, setSelectedActivity] = useState<
-    Activity | undefined
-  >(undefined);
-  const [editMode, setEditMode] = useState(false);
+    const [activities, setActivities] = useState<Activity[]>([]);
+    const [selectedActivity, setSelectedActivity] = useState<
+        Activity | undefined
+    >(undefined);
+    const [editMode, setEditMode] = useState(false);
 
-  useEffect(() => {
-    axios
-      .get<Activity[]>("http://localhost:5000/api/activities")
-      .then((response) => {
-        console.log(response);
-        setActivities(response.data);
-      });
-  }, []);
+    useEffect(() => {
+        agent.Activities.list().then((response) => {
+            const activities: Activity[] = [];
+            response.forEach((activity) => {
+                activity.date = activity.date.split("T")[0];
+                activities.push(activity);
+            });
+            setActivities(response);
+        });
+    }, []);
 
-  const handleSelectActivity = (id: string) => {
-    setSelectedActivity(activities.find((x) => x.id === id));
-  };
+    const handleSelectActivity = (id: string) => {
+        setSelectedActivity(activities.find((x) => x.id === id));
+    };
 
-  const handleCancelSelectActivity = () => {
-    setSelectedActivity(undefined);
-  };
+    const handleCancelSelectActivity = () => {
+        setSelectedActivity(undefined);
+    };
 
-  const handleFormOpen = (id?: string) => {
-    id ? handleSelectActivity(id) : handleCancelSelectActivity();
-    setEditMode(true);
-  };
+    const handleFormOpen = (id?: string) => {
+        id ? handleSelectActivity(id) : handleCancelSelectActivity();
+        setEditMode(true);
+    };
 
-  const handleFormClose = () => {
-    setEditMode(false);
-  };
+    const handleFormClose = () => {
+        setEditMode(false);
+    };
 
-  const handleCreateOrEditActivity = (activity: Activity) => {
-    activity.id
-      ? setActivities([
-          ...activities.filter((x) => x.id !== activity.id),
-          activity,
-        ])
-      : setActivities([...activities, { ...activity, id: uuid() }]);
-    setEditMode(false);
-    setSelectedActivity(activity);
-  };
+    const handleCreateOrEditActivity = (activity: Activity) => {
+        activity.id
+            ? setActivities([
+                  ...activities.filter((x) => x.id !== activity.id),
+                  activity,
+              ])
+            : setActivities([...activities, { ...activity, id: uuid() }]);
+        setEditMode(false);
+        setSelectedActivity(activity);
+    };
 
-  const handleDeleteActivity = (id: string) => {
-    setActivities([...activities.filter((x) => x.id !== id)]);
-  };
+    const handleDeleteActivity = (id: string) => {
+        setActivities([...activities.filter((x) => x.id !== id)]);
+    };
 
-  return (
-    <>
-      <NavBar openForm={handleFormOpen} />
-      <Container>
-        <ActivityDashboard
-          activities={activities}
-          selectedActivity={selectedActivity}
-          selectActivity={handleSelectActivity}
-          cancelSelectActivity={handleCancelSelectActivity}
-          editMode={editMode}
-          openForm={handleFormOpen}
-          closeForm={handleFormClose}
-          createOrEditActivity={handleCreateOrEditActivity}
-          deleteActivity={handleDeleteActivity}
-        />
-      </Container>
-    </>
-  );
+    return (
+        <>
+            <NavBar openForm={handleFormOpen} />
+            <Container>
+                <ActivityDashboard
+                    activities={activities}
+                    selectedActivity={selectedActivity}
+                    selectActivity={handleSelectActivity}
+                    cancelSelectActivity={handleCancelSelectActivity}
+                    editMode={editMode}
+                    openForm={handleFormOpen}
+                    closeForm={handleFormClose}
+                    createOrEditActivity={handleCreateOrEditActivity}
+                    deleteActivity={handleDeleteActivity}
+                />
+            </Container>
+        </>
+    );
 }
 
 export default App;
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,95 +1,101 @@
 import {
-  Button,
-  Form,
-  FormInput,
-  FormTextArea,
-  Segment,
+    Button,
+    Form,
+    FormInput,
+    FormTextArea,
+    Segment,
 } from "semantic-ui-react";
 import { Activity } from "../../../app/models/activities";
 import { ChangeEvent, useState } from "react";
 
 interface Props {
-  activity: Activity | undefined;
-  closeForm: () => void;
-  createOrEditActivity: (activity: Activity) => void;
+    activity: Activity | undefined;
+    closeForm: () => void;
+    createOrEditActivity: (activity: Activity) => void;
 }
 
 export default function ActivityForm({
-  activity: selectedActivity,
-  closeForm,
-  createOrEditActivity,
+    activity: selectedActivity,
+    closeForm,
+    createOrEditActivity,
 }: Props) {
-  const initialState = selectedActivity ?? {
-    id: "",
-    title: "",
-    category: "",
-    description: "",
-    date: "",
-    city: "",
-    venue: "",
-  };
+    const initialState = selectedActivity ?? {
+        id: "",
+        title: "",
+        category: "",
+        description: "",
+        date: "",
+        city: "",
+        venue: "",
+    };
 
-  const [activity, setActivity] = useState(initialState);
+    const [activity, setActivity] = useState(initialState);
 
-  const handleSubmit = () => {
-    console.log(activity);
-    createOrEditActivity(activity);
-  };
+    const handleSubmit = () => {
+        console.log(activity);
+        createOrEditActivity(activity);
+    };
 
-  const handleInputChange = (
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) => {
-    const { name, value } = event.target;
-    setActivity({ ...activity, [name]: value });
-  };
+    const handleInputChange = (
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => {
+        const { name, value } = event.target;
+        setActivity({ ...activity, [name]: value });
+    };
 
-  return (
-    <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete="off">
-        <FormInput
-          placeholder="Title"
-          value={activity.title}
-          name="title"
-          onChange={handleInputChange}
-        />
-        <FormTextArea
-          placeholder="Description"
-          value={activity.description}
-          name="description"
-          onChange={handleInputChange}
-        />
-        <FormInput
-          placeholder="Category"
-          value={activity.category}
-          name="category"
-          onChange={handleInputChange}
-        />
-        <FormInput
-          placeholder="Date"
-          value={activity.date}
-          name="date"
-          onChange={handleInputChange}
-        />
-        <FormInput
-          placeholder="City"
-          value={activity.city}
-          name="city"
-          onChange={handleInputChange}
-        />
-        <FormInput
-          placeholder="Venue"
-          value={activity.venue}
-          name="venue"
-          onChange={handleInputChange}
-        />
-        <Button floated="right" positive type="submit" content="Submit" />
-        <Button
-          onClick={closeForm}
-          floated="right"
-          type="button"
-          content="Cancel"
-        />
-      </Form>
-    </Segment>
-  );
+    return (
+        <Segment clearing>
+            <Form onSubmit={handleSubmit} autoComplete="off">
+                <FormInput
+                    placeholder="Title"
+                    value={activity.title}
+                    name="title"
+                    onChange={handleInputChange}
+                />
+                <FormTextArea
+                    placeholder="Description"
+                    value={activity.description}
+                    name="description"
+                    onChange={handleInputChange}
+                />
+                <FormInput
+                    placeholder="Category"
+                    value={activity.category}
+                    name="category"
+                    onChange={handleInputChange}
+                />
+                <FormInput
+                    type="date"
+                    placeholder="Date"
+                    value={activity.date}
+                    name="date"
+                    onChange={handleInputChange}
+                />
+                <FormInput
+                    placeholder="City"
+                    value={activity.city}
+                    name="city"
+                    onChange={handleInputChange}
+                />
+                <FormInput
+                    placeholder="Venue"
+                    value={activity.venue}
+                    name="venue"
+                    onChange={handleInputChange}
+                />
+                <Button
+                    floated="right"
+                    positive
+                    type="submit"
+                    content="Submit"
+                />
+                <Button
+                    onClick={closeForm}
+                    floated="right"
+                    type="button"
+                    content="Cancel"
+                />
+            </Form>
+        </Segment>
+    );
 }
