beb31b057e2ea37338603900477b4b0ddea21e80
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-07-02 15:15:03 +0200

    feat: add loading indicator

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,15 +1,31 @@
 import axios, { AxiosResponse } from "axios";
 import { Activity } from "../models/activities";
 
+const sleep = (delay: number) => {
+    return new Promise((resolve) => {
+        setTimeout(resolve, delay);
+    });
+};
+
 axios.defaults.baseURL = "http://localhost:5000/api";
+
+axios.interceptors.response.use(async (response) => {
+    try {
+        await sleep(1000);
+        return response;
+    } catch (error) {
+        console.log(error);
+        return await Promise.reject(error);
+    }
+});
 
 const responseBody = <T>(response: AxiosResponse<T>) => response.data;
 
 const requests = {
     get: <T>(url: string) => axios.get<T>(url).then(responseBody),
-    post: <T>(url: string, body: {}) =>
+    post: <T>(url: string, body: object) =>
         axios.post<T>(url, body).then(responseBody),
-    put: <T>(url: string, body: {}) =>
+    put: <T>(url: string, body: object) =>
         axios.put<T>(url, body).then(responseBody),
     del: <T>(url: string) => axios.delete<T>(url).then(responseBody),
 };
