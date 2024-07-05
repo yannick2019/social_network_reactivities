3836ff97cc4f4f46804a6a401cc8a961a329b1e9
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-07-05 15:24:49 +0200

    feat: add routes

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -1,27 +1,14 @@
-import { useEffect } from "react";
 import { Container } from "semantic-ui-react";
 import NavBar from "./NavBar";
-import ActivityDashboard from "../../features/activities/dashboard/ActivityDashboard";
-import LoadingComponent from "./LoadingComponent";
 import { observer } from "mobx-react-lite";
-import { useStore } from "../stores/store";
+import { Outlet } from "react-router-dom";
 
 function App() {
-    const { activityStore } = useStore();
-
-    useEffect(() => {
-        activityStore.loadActivities();
-    }, [activityStore]);
-
-    if (activityStore.loadingInitial) {
-        return <LoadingComponent content="Loading app" />;
-    }
-
     return (
         <>
             <NavBar />
             <Container>
-                <ActivityDashboard />
+                <Outlet />
             </Container>
         </>
     );
