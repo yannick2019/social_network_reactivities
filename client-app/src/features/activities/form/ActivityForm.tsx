df0f9c5d9b95286439d4dc6ad504dada0709b814
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-07-06 15:32:56 +0200

    redirect after submission

--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -8,9 +8,10 @@ import {
 import { ChangeEvent, useEffect, useState } from "react";
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
-import { useParams } from "react-router-dom";
+import { Link, useNavigate, useParams } from "react-router-dom";
 import { Activity } from "../../../app/models/activities";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import { v4 as uuid } from "uuid";
 
 export default observer(function ActivityForm() {
     const { activityStore } = useStore();
@@ -22,6 +23,7 @@ export default observer(function ActivityForm() {
         loadingInitial,
     } = activityStore;
     const { id } = useParams();
+    const navigate = useNavigate();
 
     const [activity, setActivity] = useState<Activity>({
         id: "",
@@ -37,11 +39,19 @@ export default observer(function ActivityForm() {
         if (id) {
             loadActivity(id).then((activity) => setActivity(activity!));
         }
-    }, [id, loadActivity, activity]);
+    }, [id, loadActivity]);
 
     const handleSubmit = () => {
-        console.log(activity);
-        activity.id ? updateActivity(activity) : createActivity(activity);
+        if (!activity.id) {
+            activity.id = uuid();
+            createActivity(activity).then(() =>
+                navigate(`/activities/${activity.id}`)
+            );
+        } else {
+            updateActivity(activity).then(() =>
+                navigate(`/activities/${activity.id}`)
+            );
+        }
     };
 
     const handleInputChange = (
@@ -51,45 +61,46 @@ export default observer(function ActivityForm() {
         setActivity({ ...activity, [name]: value });
     };
 
-    if (loadingInitial) return <LoadingComponent content="Loading..." />;
+    if (loadingInitial)
+        return <LoadingComponent content="Loading activity..." />;
 
     return (
         <Segment clearing>
             <Form onSubmit={handleSubmit} autoComplete="off">
                 <FormInput
                     placeholder="Title"
-                    value={activity?.title}
+                    value={activity.title}
                     name="title"
                     onChange={handleInputChange}
                 />
                 <FormTextArea
                     placeholder="Description"
-                    value={activity?.description}
+                    value={activity.description}
                     name="description"
                     onChange={handleInputChange}
                 />
                 <FormInput
                     placeholder="Category"
-                    value={activity?.category}
+                    value={activity.category}
                     name="category"
                     onChange={handleInputChange}
                 />
                 <FormInput
                     type="date"
                     placeholder="Date"
-                    value={activity?.date}
+                    value={activity.date}
                     name="date"
                     onChange={handleInputChange}
                 />
                 <FormInput
                     placeholder="City"
-                    value={activity?.city}
+                    value={activity.city}
                     name="city"
                     onChange={handleInputChange}
                 />
                 <FormInput
                     placeholder="Venue"
-                    value={activity?.venue}
+                    value={activity.venue}
                     name="venue"
                     onChange={handleInputChange}
                 />
@@ -100,7 +111,13 @@ export default observer(function ActivityForm() {
                     type="submit"
                     content="Submit"
                 />
-                <Button floated="right" type="button" content="Cancel" />
+                <Button
+                    as={Link}
+                    to="/activities"
+                    floated="right"
+                    type="button"
+                    content="Cancel"
+                />
             </Form>
         </Segment>
     );
