ac7b96af90df6add5d3a90c2445dbbc39cdc68d4
Yannick Yanga <52249136+yannick2019@users.noreply.github.com>
2024-07-24 22:40:08 +0200

    feat: add user entity and account controller

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+46c29f071586035da96014122e8c0e966e1188be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+177c6755d001504988d1a137750fd60f6c4782e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
